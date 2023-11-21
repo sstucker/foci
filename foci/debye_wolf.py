879972d5e7c8205569b278122c6b2f744e23d15c
sstucker <sstucker@nyu.edu>
2023-11-20 20:54:52 -0500

    fix to field gen

--- foci/debye_wolf.py
+++ foci/debye_wolf.py
@@ -19,6 +19,11 @@ import numpy as np
 from numpy import pi as PI
 import cztw
 import multiprocessing
+import warnings
+
+
+class FociWarning(UserWarning):
+    pass
 
 
 class VectorialPupil(object):
@@ -26,12 +31,17 @@ class VectorialPupil(object):
     def __init__(self, pupil_x, pupil_y=None, diameter=None, dtype=np.complex128):
         if pupil_x.shape[0] != pupil_x.shape[1]:
             raise ValueError('The pupil must be square!')
+        self._shape = (pupil_x.shape[0], pupil_x.shape[0], 2)
         if diameter is None:
             self._clear_aperture = float(pupil_x.shape[0])
         else:
             self._clear_aperture = diameter
-        self._shape = (pupil_x.shape[0], pupil_x.shape[0], 2)
         self._x = np.linspace(-self._clear_aperture / 2, self._clear_aperture / 2, self._shape[0])
+        self._aperture_mask = np.zeros((self.n, self.n), dtype=int)  # todo other aperture shapes?
+        for i, x in enumerate(self._x):
+            for j, y in enumerate(self._x):
+                if np.sqrt(x**2 + y**2) < self._clear_aperture / 2:
+                    self._aperture_mask[i, j] = 1.0
         self._pupil = np.zeros((self._shape), dtype=dtype)
         self._pupil[:, :, 0] = pupil_x
         if pupil_y is None:  # Default to linear polarization
@@ -53,33 +63,35 @@ class VectorialPupil(object):
     @property
     def x(self) -> np.ndarray:
         """Returns x-polarized component of pupil field."""
-        return self._pupil[:, :, 0]
+        return self._pupil[:, :, 0] * self._aperture_mask
     
     @property
     def y(self) -> np.ndarray:
         """Returns y-polarized component of pupil field."""
-        return self._pupil[:, :, 1]
+        return self._pupil[:, :, 1] * self._aperture_mask
     
     def set_x(self, pupil_x: np.ndarray):
-        self._pupil[:, :, 0] = pupil_x
+        self._pupil[:, :, 0] = pupil_x * self._aperture_mask
     
     def set_y(self, pupil_y: np.ndarray):
-        self._pupil[:, :, 1] = pupil_y
+        self._pupil[:, :, 1] = pupil_y * self._aperture_mask
     
     def modulus(self) -> np.ndarray:
         return np.sqrt(self.x**2 + self.y**2)
     
-    def display(self, downsample: int=12, mask_threshold: float=1E-3):
+    def display(self, downsample: int=None, mask_threshold: float=1E-3):
         """
         Display the pupil.
 
         Parameters
         ----------
         downsample : int, optional
-            The downsampled pupil used to draw polarization arrows. The default is 6.
+            The downsampled pupil used to draw polarization arrows. If None (default), the pupil polarization is displayed downsampled by factor of 16..
         mask_threshold : float, optional
             The magnitude below which the pupil magnitude is not displayed (considered negligible). The default is 1E-2.
         """
+        if downsample is None:
+            downsample = self.n // 16
         
         extent = (-self._clear_aperture / 2, self._clear_aperture / 2, -self._clear_aperture / 2, self._clear_aperture / 2)        
         dx = self._x[1] - self._x[0]
@@ -112,7 +124,7 @@ class VectorialPupil(object):
         amp = self.modulus().real
         amp[mask_im] = np.nan
         plt.imshow(amp, extent=extent, cmap='Reds', vmin=0)
-        ax1.quiver(X[mask], Y[mask], dx * downsample / 6 * u[mask] / a[mask], dx * downsample / 6 * v[mask] / a[mask], color='black', width=dx * 1E5, alpha=0.6)
+        ax1.quiver(X[mask], Y[mask], dx * downsample / 6 * u[mask] / a[mask], dx * downsample / 6 * v[mask] / a[mask], color='black', alpha=0.6)
         ax1.set_aspect('equal')
         ax1.set_xlim(-self._clear_aperture / 1.9, self._clear_aperture / 1.9)
         ax1.set_ylim(-self._clear_aperture / 1.9, self._clear_aperture / 1.9)
@@ -130,15 +142,15 @@ class VectorialPupil(object):
         plt.imshow(ph, extent=extent, cmap='Blues', vmin=-2*PI, vmax=2*PI)
         for _x, _y, _ku, _kv, _ka in zip(X[mask].flatten(), Y[mask].flatten(), ku[mask].flatten(), kv[mask].flatten(), ka[mask].flatten()):
             dp = np.abs(_ku - _kv) % PI
-            tilt = 180/PI * dp / 2 + 45 # degrees
+            tilt = 180/PI * dp / 2 + 45  # degrees
             h = 1
             w = np.sin(dp)
             ax2.add_patch(Ellipse((_x, _y), h * dx * downsample / 1.5, w * dx * downsample / 1.5, angle=tilt, facecolor='none', edgecolor='black', linewidth=0.2))
             if dp > mask_threshold:
                 if _ku < _kv: # If left-handed
-                    marker='o'
+                    marker='$l$'
                 else:
-                    marker='x'
+                    marker='$r$'
                 ax2.scatter([_x], [_y], s=8, color='black', marker=marker)
         ax2.set_aspect('equal')
         ax2.set_xlim(-self._clear_aperture / 1.9, self._clear_aperture / 1.9)
@@ -166,12 +178,14 @@ def elliptical_polarize(pupil: VectorialPupil, dphase: float=PI/2) -> VectorialP
         The polarized pupil instance.
 
     """
-    px = pupil.x
-    pupil.set_x(px.real + 1j * (px.imag + dphase))
+    shifted = pupil.x * np.exp(1j * dphase)
+    pupil.set_x(shifted)
+    if np.max(pupil.y.real) == 0 or np.max(pupil.x.real) == 0:
+        warnings.warn('Only one component of the pupil is nonzero: the resulting pupil is not elliptical!', FociWarning)
     return pupil
 
 
-def vortex_polarize(pupil: VectorialPupil, angle: float=0) -> VectorialPupil:
+def vortical_polarize(pupil: VectorialPupil, angle: float=0) -> VectorialPupil:
     """
     Applies vortical polarization to the modulus field of the pupil (existing polarization is lost). 
 
@@ -191,9 +205,9 @@ def vortex_polarize(pupil: VectorialPupil, angle: float=0) -> VectorialPupil:
     n = pupil.n
     xx, yy = np.mgrid[-1:1:n*1j, -1:1:n*1j]
     phi = np.arctan2(xx, yy) + angle
-    p = pupil.modulus().real
-    pupil.set_x(np.cos(phi) * p + pupil.x.imag)
-    pupil.set_y(np.sin(phi) * p + pupil.y.imag)
+    p = pupil.modulus()
+    pupil.set_x(np.cos(phi) * p)
+    pupil.set_y(np.sin(phi) * p)
     return pupil
 
 
@@ -212,13 +226,12 @@ def ring_beam(N, radius, waist):
 
 
 # TODO shift so that mm is 1.0
-NM = 10**-9
-UM = 10**-6
-MM = 10**-3
-M = 1.0
+MM = 1000.0
+UM = 1.0
+NM = 1.0 / 1000.0
 
 # Lens parameters
-PUPIL_DIAMETER = 18 * MM
+PUPIL_DIAMETER = 25 * MM
 FOCAL_LENGTH = 12.5 * MM
 WAVELENGTH = 750 * NM 
 
@@ -229,16 +242,16 @@ FIELD_INDEX = 1.33  # Water
 
 # Simulation resolution
 N = 256
-Z = 256
+Z = 128
 
-# pupil = VectorialPupil(ring_beam(N, N // 3, 4), diameter=FOV_WIDTH)
-pupil = VectorialPupil(np.ones((N, N)), diameter=FOV_WIDTH)
+pupil = VectorialPupil(ring_beam(N, N // 3, 1), diameter=PUPIL_DIAMETER)
+# pupil = VectorialPupil(np.zeros((N, N)), pupil_y=np.ones((N, N)), diameter=PUPIL_DIAMETER)
+# pupil = VectorialPupil(np.ones((N, N)), pupil_y=np.zeros((N, N)), diameter=PUPIL_DIAMETER)
+# pupil = VectorialPupil(np.ones((N, N)), pupil_y=np.ones((N, N)), diameter=PUPIL_DIAMETER)
 # pupil.display()
 
-
-# pupil = VectorialPupil(pupil_x=px, pupil_y=py, diameter=FOV_WIDTH)
-pupil = elliptical_polarize(pupil, dphase=PI/2)
-# pupil = vortex_polarize(pupil, angle=-PI)
+pupil = vortical_polarize(pupil, angle=PI/2)
+# pupil = elliptical_polarize(pupil, dphase=PI/2)
 
 pupil.display()
 
@@ -249,7 +262,7 @@ numerical_aperture = FIELD_INDEX * np.sin(angle_of_convergence)
 
 # Pixel size
 px_per_m = N / FOV_WIDTH
-# dz = FOV_DEPTH / Z
+dz = FOV_DEPTH / Z
 
 k = FIELD_INDEX * 2 * PI / WAVELENGTH  # Wavenumber
 
@@ -263,8 +276,10 @@ kxx, kyy = np.meshgrid(kx, kx)  # Cartesian grid
 krxy = np.sqrt(kxx**2 + kyy**2)  # Radial k-space grid
 
 # k-space coordinates of z (distance to spherical cap)
-kzxy = np.sqrt(dk**2 - krxy**2)
-# kzxy[np.isnan(kzxy)] = 0
+kzxy = np.sqrt(dk**2 - krxy**2)  # Floor to zero
+kzxy[np.isnan(kzxy)] = 0
+# z = dk**2 - krxy**2
+# kzxy = np.sqrt(np.where(z < 0, 0, z))  # Floor to zero
 
 thetaxy = np.arctan2(krxy, kzxy)  # angles made with optical axis
 phixy = np.arctan2(kyy, kxx)  # angles made with a plane transverse the optical axis
@@ -274,7 +289,7 @@ theta_mask = thetaxy < angle_of_convergence  # Mask of theta within the pupil
 # Real operators from pupil function to spherical k-space (as in [1])
 Gx = np.sqrt(np.cos(thetaxy)) *\
     (np.cos(thetaxy) + (1 - np.cos(thetaxy)) *
-    np.sin(phixy) ** 2) / np.cos(thetaxy)
+    np.sin(phixy)**2) / np.cos(thetaxy)
     
 Gy = np.sqrt(np.cos(thetaxy)) *\
     ((np.cos(thetaxy) - 1) * np.cos(phixy) *
@@ -283,11 +298,17 @@ Gy = np.sqrt(np.cos(thetaxy)) *\
 Gz = -np.sqrt(np.cos(thetaxy)) *\
     np.sin(thetaxy) * np.cos(phixy) / np.cos(thetaxy)
 
-czt = cztw.plan(2, N, w0=-k_bandwidth, w1=k_bandwidth, precision='complex64')
+czt = cztw.plan(2, N, w0=-k_bandwidth, w1=k_bandwidth, precision='complex128')
 
 slices = []
 
-for dz in np.linspace(-FOV_DEPTH / 2, FOV_DEPTH / 2, Z):
+if Z > 1:
+    zd = np.linspace(0, FOV_DEPTH / 2, Z // 2 + 1)
+    depths = np.concatenate((-zd[:0:-1], zd[:-1]))
+else:
+    depths = [0]
+
+for dz in depths:
     
     # Each depth experiences additional free space propagation
     propagation_tf = np.exp(2j * PI * kzxy * dz * px_per_m)
@@ -304,13 +325,25 @@ for dz in np.linspace(-FOV_DEPTH / 2, FOV_DEPTH / 2, Z):
     E_Yx = czt(-l_0y * np.rot90(Gy))
     E_Yy = czt(l_0y * np.rot90(Gx))
     E_Yz = czt(l_0y * np.rot90(Gz))
-    
+
     E_X = E_Xx + E_Yx
     E_Y = E_Xy + E_Yy
     E_Z = E_Xz + E_Yz
     
-    intensity = np.abs(E_X)**2 + np.abs(E_Y)**2 + np.abs(E_Z)**2
+    I_X = np.abs(E_X)**2
+    I_Y = np.abs(E_Y)**2
+    I_Z = np.abs(E_Z)**2
     
+    # plt.figure('Debug: I_X, I_Y, I_Z')
+    # plt.subplot(1, 3, 1)
+    # plt.imshow(I_X)
+    # plt.subplot(1, 3, 2)
+    # plt.imshow(I_Y)
+    # plt.subplot(1, 3, 3)
+    # plt.imshow(I_Z)
+
+    intensity = I_X + I_Y + I_Z
+
     slices.append(intensity)
     
 psf = np.array(slices)
